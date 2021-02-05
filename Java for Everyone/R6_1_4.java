2290f911f4dcbce0bbebf4c9c898837581bd0f0b
Marcus3W <60431130+Marcus3W@users.noreply.github.com>
2021-02-05 11:19:54 +0000

    Update R6_1_4.java

--- Java for Everyone/R6_1_4.java
+++ Java for Everyone/R6_1_4.java
@@ -4,12 +4,11 @@
 
 import java.util.Arrays;
 
-public class R6_1 {
+public class R6_1_4 {
 
     public static void main (String[] args)
     {
         int[] array = new int[10];
-
         System.out.println(Arrays.toString(array));
     }
 }
