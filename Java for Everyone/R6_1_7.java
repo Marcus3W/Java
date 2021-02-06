a653894db8c935b406b27ffb7adffb854f4fdd1b
Marcus3W <60431130+Marcus3W@users.noreply.github.com>
2021-02-06 10:58:17 +0000

    Create R6_1_7.java

---
+++ Java for Everyone/R6_1_7.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/** Write code that fills an array values with each set of numbers below.
+ *  0 1 2 3 4 0 1 2 3 4
+ *
+ */
+
+public class R6_1 {
+
+    public static void main (String[] args)
+    {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = i % 5;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
