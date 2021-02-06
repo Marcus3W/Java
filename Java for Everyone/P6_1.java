5f0c7e115424bd35d752aeb32cddce903d4daf82
Marcus3W <60431130+Marcus3W@users.noreply.github.com>
2021-02-06 14:28:06 +0000

    Create P6_1.java

---
+++ Java for Everyone/P6_1.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+/*Write a program that initializes an array with ten random integers and then prints four lines of output, containing;
+•    Every element at an even index.
+•    Every even element.
+•    All elements in reverse order.
+•    Only the first and last element.*/
+
+public class P6_1
+{
+
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random() * 100 + 1);
+
+        }
+        System.out.println(("Original Array: ") + (Arrays.toString(array)));
+
+
+        System.out.println("\n" + "Every even index element");
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println("");
+
+        System.out.println("\n" + "Every even element");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println("");
+
+        System.out.println("\n" + "All elements in reverse order");
+        for (int i = array.length - 1 ; i > -1 ; i--)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("");
+
+        System.out.println("\n" + "Only the first and last elements");
+        {
+            System.out.print(array[0] + " ");
+            System.out.print(array[array.length - 1] + " ");
+        }
+        System.out.println("");
+    }
+    
+}
+
+
