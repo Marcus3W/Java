a0dbea938ea11a0bf09dba4b1a914a50af7a6906
Marcus3W <60431130+Marcus3W@users.noreply.github.com>
2021-02-06 14:32:54 +0000

    Update P6_1.java

--- Java for Everyone/P6_1.java
+++ Java for Everyone/P6_1.java
@@ -8,18 +8,16 @@ import java.util.Arrays;
 
 public class P6_1
 {
-
     public static void main(String[] args) {
 
+        // Set up initial array
         int[] array = new int[10];
-
         for(int i = 0; i < array.length; i++) {
             array[i] = (int)(Math.random() * 100 + 1);
-
         }
         System.out.println(("Original Array: ") + (Arrays.toString(array)));
 
-
+        // Print even index elements
         System.out.println("\n" + "Every even index element");
         for (int i = 0; i < array.length; i++) {
             if (i % 2 == 0) {
@@ -28,6 +26,7 @@ public class P6_1
         }
         System.out.println("");
 
+        // Print even elements
         System.out.println("\n" + "Every even element");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
@@ -36,6 +35,7 @@ public class P6_1
         }
         System.out.println("");
 
+        // Print elements in reverse order
         System.out.println("\n" + "All elements in reverse order");
         for (int i = array.length - 1 ; i > -1 ; i--)
         {
@@ -43,14 +43,13 @@ public class P6_1
         }
         System.out.println("");
 
+        // Print only the first and last elements
         System.out.println("\n" + "Only the first and last elements");
         {
             System.out.print(array[0] + " ");
             System.out.print(array[array.length - 1] + " ");
         }
         System.out.println("");
+        
     }
-    
 }
-
-
