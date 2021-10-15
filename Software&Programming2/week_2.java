b58f5ac278e1c6c2e2786e3139fb5ad77bd2c380
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-10-15 15:42:13 +0100

    Update week_2.java
    Added question 5

--- Software&Programming2/week_2.java
+++ Software&Programming2/week_2.java
@@ -52,7 +52,6 @@ public class week2 {
         // if the position is even and more than 10 after doubling
         return (sum / 10) + (sum % 10);
     }
-
     /**
      * QUESTION 4 - check card number that is passed. Reuse Q3 decode method
      *
@@ -91,6 +90,46 @@ public class week2 {
         return card_length;
     }
 
+    /**
+     * QUESTION 5 - Perform a series of tasks
+     *
+     * (a) Create an array x of doubles with an initializer list (see also the code for Question 6) that contains the following values: 8, 4, 5, 21, 7, 9, 18, 2, and 100.
+     * (b) Print the number of items in the array by using an expression of the form x.length.
+     * (c) Print the first array item, x[0].
+     * (d) Print the last array item. Be careful to choose the right index.
+     * (e) Print the expression x[x.length - 1].
+     * (f) Use a standard for loop to print all the values in the array without labels.
+     * (g) Use a standard for loop to print all the values in the array with labels ("x[0]", "x[1]", . . . ) to indicate what each element is.
+     * (h) Use a standard for loop to print all the values in the array in reverse order with labels to indicate what each element is.
+     * (i) Use an enhanced for loop to print all the values in the array without labels.
+     */
+    public static void questionSix() {
+        double[] x = {8, 4, 5, 21, 7, 9, 18, 2, 100};   //a)
+        System.out.println(x.length);                   //b)
+        System.out.println(x[0]);                       //c)
+        System.out.println(x[x.length-1]);              //d)
+        System.out.println(x[x.length-1]); ;            //e)
+        for (int i = 0; i < x.length; i++) {            //f)
+            System.out.println(x[i] + ", ");
+        }
+        for (int i = 0; i < x.length; i++) {            //g)
+            System.out.println("x["+i+"] = " + x[i]);
+        }
+        for (int i = x.length-1; i >= 0; i--) {         //h)
+            System.out.println("x["+i+"] = " + x[i]);
+        }
+        for (double element : x) {
+            System.out.println(element);
+        }
+
+    }
+
+
+
+
+
+
+
 
 
     public static void main(String[] args) {
@@ -118,6 +157,7 @@ public class week2 {
         System.out.println("CC number: " + num + " is " + checkDigits(num));
         */
 
+        questionSix();
 
     }
 
