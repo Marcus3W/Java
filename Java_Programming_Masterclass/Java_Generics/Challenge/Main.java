67640177a6f3f697752ffd46ca3536a966a5b405
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-11 10:38:43 +0000

    Generics Challenge - Main class update

--- Java_Programming_Masterclass/Java_Generics/Challenge/Main.java
+++ Java_Programming_Masterclass/Java_Generics/Challenge/Main.java
@@ -1,5 +1,7 @@
 package Java_Generics.Challenge;
 
+import java.util.Collection;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -20,6 +22,29 @@ public class Main {
         premierLeague.addTeam(brentford);
         //premierLeague.addTeam(leeds); Generic test. Doesn't allow me to add from a different type
         System.out.println(premierLeague.numTeams() + " teams are in the " + premierLeague.getName());
+
+        // MATCHES
+        astonVilla.matchResult(fulham, 1, 3); // villa 0
+        astonVilla.matchResult(brentford, 0, 2); // villa 0
+
+        fulham.matchResult(brentford, 1, 1); // fulham 4
+        fulham.matchResult(astonVilla, 3, 2); // fulam 7
+
+        brentford.matchResult(astonVilla, 1,0); // brentford 7
+        brentford.matchResult(fulham, 5,0); // brentford 10
+
+        System.out.println(astonVilla.getName() + " total points: " + astonVilla.ranking());  //Expected: Aston Villa 0
+        System.out.println(fulham.getName() + " total points: " + fulham.ranking());  // Fulham 7
+        System.out.println(brentford.getName() + " total points: " + brentford.ranking());  // Brentford 10
+
+        premierLeague.showLeagueTable();
+
+
+
+
+
+
+
 
         System.out.println();
         System.out.println("Rugby League Creation:");
