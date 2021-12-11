d4c65ed7243653c718759a1f77593d17d0ec0ce4
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-11 10:38:20 +0000

    Generics Challenge - league update

--- Java_Programming_Masterclass/Java_Generics/Challenge/League.java
+++ Java_Programming_Masterclass/Java_Generics/Challenge/League.java
@@ -1,16 +1,13 @@
 package Java_Generics.Challenge;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
-public class League<T extends Team> implements Comparable<League<T>> {
+public class League<T extends Team> {
 
     private String name;
-    int played = 0;
-    int won = 0;
-    int lost = 0;
-    int drew = 0;
-
-    private ArrayList<T> teams = new ArrayList<>();
+    private ArrayList<T> league = new ArrayList<>();
 
     public League(String name) {
         this.name = name;
@@ -21,54 +18,30 @@ public class League<T extends Team> implements Comparable<League<T>> {
     }
 
     public boolean addTeam(T team) {
-        if (teams.contains(team)) {
+        if (league.contains(team)) {
             System.out.println(team.getName() + " is already in this league");
             return false;
         } else {
-            teams.add(team);
+            league.add(team);
             System.out.println(team.getName() + " added to " + this.name);
             return true;
         }
     }
 
     public int numTeams() {
-        return this.teams.size();
+        return this.league.size();
     }
 
-    public void matchResult(League<T> opponent, int homeScore, int awayScore) {
-        String message;
-
-        if (homeScore > awayScore) {
-            won++;
-            message = " beat ";
-        } else if (homeScore == awayScore) {
-            drew++;
-            message = " drew with ";
-        } else {
-            lost++;
-            message = " lost to ";
-        }
-        played++;
-        if (opponent != null) {
-            System.out.println(this.getName() + message + opponent.getName());
-            opponent.matchResult(null, awayScore, homeScore);
+    public void showLeagueTable() {
+        Collections.sort(league);
+        for(T t : league) {
+            System.out.println(t.getName() + ": " + t.ranking());
         }
     }
 
-    public int ranking() {
-        return (won * 3) + drew;
-    }
 
-    @Override
-    public int compareTo(League<T> league) {
-        if (this.ranking() > league.ranking()) {
-            return -1;
-        }
-        else {
-            if (this.ranking() < league.ranking()) {
-                return 1;
-            }
-        }
-        return 0;
-    }
+
+
+
+
 }
