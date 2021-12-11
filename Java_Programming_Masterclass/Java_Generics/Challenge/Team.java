ec164e4274aaa0c9e1b124788f2a4f2abafed1c1
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-11 10:38:31 +0000

    Generics Challenge - Team class update

--- Java_Programming_Masterclass/Java_Generics/Challenge/Team.java
+++ Java_Programming_Masterclass/Java_Generics/Challenge/Team.java
@@ -1,8 +1,11 @@
 package Java_Generics.Challenge;
 
-public class Team {
-
+public abstract class Team<T> implements Comparable<Team<T>> {
     private String name;
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int drew = 0;
 
     public Team(String name) {
         this.name = name;
@@ -11,4 +14,41 @@ public class Team {
     public String getName() {
         return name;
     }
+
+    public void matchResult(Team<T> opponent, int homeScore, int awayScore) {
+        String message;
+
+        if (homeScore > awayScore) {
+            won++;
+            message = " beat ";
+        } else if (homeScore == awayScore) {
+            drew++;
+            message = " drew with ";
+        } else {
+            lost++;
+            message = " lost to ";
+        }
+        played++;
+        if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
+            opponent.matchResult(null, awayScore, homeScore);
+        }
+    }
+    public int ranking() {
+        return (won * 3) + drew;
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        if (ranking() > team.ranking()) {
+            return -1;
+        }
+        else {
+            if (this.ranking() < team.ranking()) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
 }
