8edd2d7801f2606c59c26296df08111e94fea763
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-14 17:20:50 +0000

    JUnit package and Banking classes created

--- Java_Programming_Masterclass/Debugging_and_Unit_Testing/Banking/BankAccountTest.java
+++ Java_Programming_Masterclass/Debugging_and_Unit_Testing/Banking/BankAccountTest.java
@@ -9,7 +9,9 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
+        double balance = account.deposit(1000.00, true);
+        assertEquals(1500.00, balance, 0); // third param is delta, gives a 'leeway' on double figures
     }
 
     @org.junit.jupiter.api.Test
@@ -18,9 +20,28 @@ class BankAccountTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail("This test has yet to be implemented");
+    void getBalance_deposit() {
+        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
+        account.deposit(1000.00, true);
+        assertEquals(1500.00, account.getBalance(), 0);
     }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(300.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void isChecking_true() {
+        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
+        assertTrue(account.isCHECKING(), "The account is NOT a checking account");
+        // when checking boolean, use assertTrue or assertFalse, instead of assertEquals
+    }
+
+
+
 
     // ASSERT - state a fact or belief confidently and forcefully.
     //        - The first parameter is what we expect, and the second is what we want to test / actual value
