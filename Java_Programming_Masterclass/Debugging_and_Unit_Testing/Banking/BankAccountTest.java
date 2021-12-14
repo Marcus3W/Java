08553b5d8b7fb425ffed6f87afff26d2ffd2b36f
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-14 17:53:38 +0000

    JUnit package and Banking classes created

--- Java_Programming_Masterclass/Debugging_and_Unit_Testing/Banking/BankAccountTest.java
+++ Java_Programming_Masterclass/Debugging_and_Unit_Testing/Banking/BankAccountTest.java
@@ -7,9 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class BankAccountTest {
 
+    private BankAccount account;
+
+    /**
+     *  MAYBER WE ONLY WANT TO RUN THE METHOD ONCE, UNLIKE THE BELOW WHERE ITS RUN EACH TIME BEFORE EACH TEST
+     *  BeforeAll – denotes that the annotated method will be executed before all test
+     *              methods in the current class (previously @BeforeClass)
+     */
+    @org.junit.jupiter.api.BeforeAll
+    public static void beforeClass() {
+        System.out.println("This executes before any test cases");
+    }
+
+    /**
+     *  CREATED THIS SO WE CAN CREATE THE OBJECT INSTEAD OF HAVING IT CREATING IN EACH TEST
+     *  BeforeEach – denotes that the annotated method will be executed before each test method (previously @Before)
+     */
+    @org.junit.jupiter.api.BeforeEach
+    public void setup() {
+        account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
+        System.out.println("Running a test");
+    }
+
+
+
     @org.junit.jupiter.api.Test
     void deposit() {
-        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
         double balance = account.deposit(1000.00, true);
         assertEquals(1500.00, balance, 0); // third param is delta, gives a 'leeway' on double figures
     }
@@ -21,27 +44,21 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void getBalance_deposit() {
-        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
         account.deposit(1000.00, true);
         assertEquals(1500.00, account.getBalance(), 0);
     }
 
     @org.junit.jupiter.api.Test
     void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(300.00, account.getBalance(), 0);
     }
 
     @org.junit.jupiter.api.Test
     void isChecking_true() {
-        BankAccount account = new BankAccount("Marc", "Wheeler", 500.00, BankAccount.CHECKING);
         assertTrue(account.isCHECKING(), "The account is NOT a checking account");
         // when checking boolean, use assertTrue or assertFalse, instead of assertEquals
     }
-
-
-
 
     // ASSERT - state a fact or belief confidently and forcefully.
     //        - The first parameter is what we expect, and the second is what we want to test / actual value
@@ -51,6 +68,13 @@ class BankAccountTest {
 
     }
 
-
+    /**
+     * AfterAll – denotes that the annotated method will be executed after all
+     *             test methods in the current class (previously @AfterClass)
+     */
+    @org.junit.jupiter.api.AfterAll
+    public static void AfterClass() {
+        System.out.println("This executes AFTER any test cases");
+    }
 
 }
