ac1ba7fe79deeba838538f7d9e6fbbcb9017508f
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-18 17:18:20 +0000

    Codecademy - Encapsulation - Person class added

---
+++ Codecademy/Classes/Access_Encapsulation_StaticMethods/Person.java
@@ -0,0 +1,37 @@
+package Classes.Access_Encapsulation_StaticMethods;
+
+public class Person{
+    public int age;
+    public int wisdom;
+    public int fitness;
+
+    public Person(int inputAge){
+        this.age = inputAge;
+        this.wisdom = inputAge * 5;
+        this.fitness = 100 - inputAge;
+    }
+
+    public void setAge(int newAge){
+        this.age = newAge;
+    }
+
+    public void setWisdom(int newWisdom){
+        this.wisdom = newWisdom;
+    }
+
+    public void setFitness(int newFitness){
+        this.fitness = newFitness;
+    }
+
+    public void hasBirthday(){
+        //Complete this method
+    }
+
+    public static void main(String[] args){
+        Person emily = new Person(20);
+        emily.hasBirthday();
+        System.out.println("New age is: " + emily.age);
+        System.out.println("New wisdom is: " + emily.wisdom);
+        System.out.println("New fitness is: " + emily.fitness);
+    }
+}
