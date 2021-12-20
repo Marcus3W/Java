6285c2a97012805157f2f76492e25b1a65297a6a
Marcus3W <marcus3w@live.com>
2021-12-20 21:29:59 +0000

    Codecademy - Inheritance - Exercises

--- Codecademy/Classes/Inheritance_and_Polymorphism/Noodle.java
+++ Codecademy/Classes/Inheritance_and_Polymorphism/Noodle.java
@@ -2,11 +2,11 @@ package Classes.Inheritance_and_Polymorphism;
 
 class Noodle {
 
-    double lengthInCentimeters;
-    double widthInCentimeters;
-    String shape;
-    String ingredients;
-    String texture = "brittle";
+    protected double lengthInCentimeters;
+    protected double widthInCentimeters;
+    protected String shape;
+    protected String ingredients;
+    protected String texture = "brittle";
 
     Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
 
@@ -19,14 +19,16 @@ class Noodle {
 
     public void cook() {
 
+        System.out.println("Boiling.");
+
         this.texture = "cooked";
 
     }
 
     public static void main(String[] args) {
-        Pho phoChay = new Pho();
-        System.out.println(phoChay.shape);
 
+        Spaetzle kaesespaetzle = new Spaetzle();
+        kaesespaetzle.cook();
 
     }
 
---
+++ Codecademy/Classes/Inheritance_and_Polymorphism/Ramen.java
@@ -0,0 +1,11 @@
+package Classes.Inheritance_and_Polymorphism;
+
+public class Ramen extends Noodle {
+
+    Ramen() {
+
+        super(30.0, 0.3, "flat", "wheat flour");
+
+    }
+
+}
