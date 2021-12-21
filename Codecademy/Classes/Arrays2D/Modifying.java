708e3af72aec133ba49b4d97fc8b9cd032333eb9
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-21 22:06:24 +0000

    Codecademy - 2D Arrays

--- Codecademy/Classes/Arrays2D/Modifying.java
+++ Codecademy/Classes/Arrays2D/Modifying.java
@@ -11,16 +11,21 @@ public class Modifying {
         };
 
         // Replace the number 4 in the 2D array with the number 0
-
+        intMatrix[1][1] = 0;
 
         // Declare and initialize a new empty 2x2 integer 2D array called subMatrix
-
+        int[][] subMatrix = new int[2][2];
 
         // Using 4 lines of code, multiply each of the elements in the 2x2 top left corner of intMatrix by 5 and store the results in the subMatrix you created. Afterwards, uncomment the provided print statement below.
+        subMatrix[0][0] = intMatrix[0][0] * 5;
+        subMatrix[0][1] = intMatrix[0][1] * 5;
+        subMatrix[1][0] = intMatrix[1][0] * 5;
+        subMatrix[1][1] = intMatrix[1][1] * 5;
 
 
 
 
-        //System.out.println(Arrays.deepToString(subMatrix));
+
+        System.out.println(Arrays.deepToString(subMatrix));
     }
 }
