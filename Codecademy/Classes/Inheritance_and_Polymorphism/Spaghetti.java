efb5f5404e7839a57f1cdd724eb21947aa90a120
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2021-12-21 11:15:20 +0000

    Codecademy - Static Methods / Variables - ATM

--- Codecademy/Classes/Inheritance_and_Polymorphism/Spaghetti.java
+++ Codecademy/Classes/Inheritance_and_Polymorphism/Spaghetti.java
@@ -2,8 +2,7 @@ package Classes.Inheritance_and_Polymorphism;
 
 class Spaghetti extends Noodle {
 
-
-
-
-
+    Spaghetti(double lenInCent, double wthInCent, String shp, String ingr) {
+        super(lenInCent, wthInCent, shp, ingr);
+    }
 }
