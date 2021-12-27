ae1cf6a790d6a2dae40b0ed479310c814879434a
Marcus3W <marcus3w@live.com>
2021-12-27 11:41:21 +0000

    Codecademy - Passing the technical interview path - Node created and runs

--- Codecademy/Pass_The_Technical_Interview/Node.java
+++ Codecademy/Pass_The_Technical_Interview/Node.java
@@ -2,19 +2,34 @@ package Pass_The_Technical_Interview;
 
 public class Node {
 
-    public String data;
-    public Node next;
+    private String data;
+    private Node next;
 
     public Node(String data) {
         this.data = data;
         this.next = null;
     }
 
+    public void setNextNode(Node node) {
+        this.next = node;
+    }
+
+    public Node getNextNode() {
+        return this.next;
+    }
 
     public static void main(String[] args) {
-
+        /* TESTING
         Node firstNode = new Node("I am a Node!");
         System.out.println(firstNode.data);
         System.out.println(firstNode.next);
+        Node secondNode = new Node("I am the second Node!");
+        firstNode.setNextNode(secondNode);
+        System.out.println(firstNode.next.data);
+        System.out.println(firstNode.getNextNode().data);
+        */
+
+
+
     }
 }
