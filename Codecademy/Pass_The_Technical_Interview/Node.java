784087d078233d74ae59f0f69c4ad76fc0d7003a
Marcus3W <marcus3w@live.com>
2021-12-27 11:27:59 +0000

    Codecademy - Passing the technical interview path

---
+++ Codecademy/Pass_The_Technical_Interview/Node.java
@@ -0,0 +1,21 @@
+package Pass_The_Technical_Interview;
+
+public class Node {
+
+    public String data;
+    public Node next;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
