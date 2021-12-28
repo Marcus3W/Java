0b6e8eb4e68f8538fef51d67e9ffe7c1f05b37a8
Marcus3W <marcus3w@live.com>
2021-12-28 22:44:21 +0000

    Codility - Lessons

--- Codility/Lesson1.java
+++ Codility/Lesson1.java
@@ -1,2 +1,25 @@
 public class Lesson1 {
+
+    public static int solution(int n) {
+        String binary = getBinary(n);
+
+        while (binary.length() > 0) {
+
+        }
+        return -1;
+
+    }
+
+    public static String getBinary(int n) {
+        String s = "";
+        while (n > 0) {
+            s = ((n % 2) == 0 ? "0" : "1") + s;
+            n = n / 2;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        solution(32);
+    }
 }
