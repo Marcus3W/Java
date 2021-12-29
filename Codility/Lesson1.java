92709b9f8cbd2ef02244abb34c7401422552ea3b
Marcus3W <marcus3w@live.com>
2021-12-29 17:03:28 +0000

    Codility - Lessons

--- Codility/Lesson1.java
+++ Codility/Lesson1.java
@@ -1,25 +1,47 @@
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lesson1 {
-
     public static int solution(int n) {
-        String binary = getBinary(n);
-
-        while (binary.length() > 0) {
-
+        int finalGap = 0;
+        if (n <= 1) {
+            return 0;
         }
-        return -1;
-
+        char[] binaryRep = Integer.toBinaryString(n).toCharArray();
+        int tempGap = 0;
+        for (char c : binaryRep) {
+            if (c == '0') {
+                tempGap++;
+            } else if (c == '1') {
+                if (tempGap > finalGap) {
+                    finalGap = tempGap;
+                }
+                tempGap = 0;
+            }
+        }
+        return finalGap;
     }
 
-    public static String getBinary(int n) {
-        String s = "";
-        while (n > 0) {
-            s = ((n % 2) == 0 ? "0" : "1") + s;
-            n = n / 2;
-        }
-        return s;
+    public static void verifySolution(int pInput, int expectedBinaryGap) {
+        Assert.assertEquals(solution(pInput), expectedBinaryGap);
     }
 
     public static void main(String[] args) {
-        solution(32);
+        verifySolution(0, 0);
+        verifySolution(9, 2);
+        verifySolution(15, 0);
+        verifySolution(32, 0);
+        verifySolution(529, 4);
+        verifySolution(1041, 5);
+        verifySolution(65536, 0);
+        verifySolution(65537, 15);
+        verifySolution(100000, 4);
+        verifySolution(2147483, 5);
+        verifySolution(2147483637, 1);
+        verifySolution(2147483646, 0);
+        verifySolution(2147483647, 0);
+
     }
 }
