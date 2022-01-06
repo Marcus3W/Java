48bdecf1c3c77450485ffad261bbe91da5c0338e
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2022-01-06 14:30:52 +0000

    Pong game

--- Games/Pong/Ball.java
+++ Games/Pong/Ball.java
@@ -5,8 +5,8 @@ public class Ball {
     public Rect rect;
     public Rect leftPaddle, rightPaddle;
 
-    private double vy = -1.0;
-    private double vx = -1.0;
+    private double vy = 10.0;
+    private double vx = -200.0;
     // velocity x, y
 
     public Ball(Rect rect, Rect leftPaddle, Rect rightPaddle) {
@@ -15,37 +15,55 @@ public class Ball {
         this.rightPaddle = rightPaddle;
     }
 
-    public void update(double dt) {
+    public double calculateNewVelocityAngle(Rect paddle) {
+        double relativeIntersectY = (paddle.y + (paddle.height / 2.0)) - (this.rect.y + (this.rect.height / 2.0));
+        double normalIntersectY = relativeIntersectY / (paddle.height / 2.0);
+        double theta = normalIntersectY * Constants.MAX_ANGLE;
 
+        return Math.toRadians(theta);
+    }
+
+    public void update(double dt) {
         if (vy >= 0.0) {
-            if (this.rect.y + (vy * Constants.BALL_SPEED * dt) + this.rect.height > Constants.SCREEN_HEIGHT - Constants.INSETS_BOTTOM) {
+            if (this.rect.y + (vy * dt) + this.rect.height > Constants.SCREEN_HEIGHT - Constants.INSETS_BOTTOM) {
                 vy *= -1.0;
             }
         } else if (vy < 0.0) {
-            if (this.rect.y +(vy * Constants.BALL_SPEED * dt) < Constants.TOOLBAR_HEIGHT) {
+            if (this.rect.y +(vy * dt) < Constants.TOOLBAR_HEIGHT) {
                 vy *= -1.0;
             }
         }
 
         if (vx < 0.0) {
-            if (this.rect.x + (vx * Constants.BALL_SPEED * dt) + leftPaddle.x > leftPaddle.width) {
-                if (this.rect.y + (vy * Constants.BALL_SPEED * dt) > leftPaddle.y &&
-                        this.rect.y + (vy * Constants.BALL_SPEED * dt) + this.rect.height < leftPaddle.y + leftPaddle.height) {
-                    vx *= -1.0;
-                }
+            if (this.rect.x + (vx *  dt) < leftPaddle.x + leftPaddle.width) {
+                if (this.rect.y + (vy *  dt) > leftPaddle.y &&
+                        this.rect.y + (vy * dt) + this.rect.height < leftPaddle.y + leftPaddle.height) {
+                    double theta = calculateNewVelocityAngle(leftPaddle);
+                    double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
+                    double newVy = (-Math.sin(theta)) * Constants.BALL_SPEED;
 
-            } else if (vx >= 0) {
-                if (this.rect.x + (vx * Constants.BALL_SPEED * dt) + rect.width > rightPaddle.x) {
-                    if (this.rect.y + (vy * Constants.BALL_SPEED * dt) > rightPaddle.y &&
-                            this.rect.y + (vy * Constants.BALL_SPEED * dt) + this.rect.height < rightPaddle.y + rightPaddle.height) {
-                        vx *= -1.0;
+                    double oldSign = Math.signum(vx);
+                    this.vx = newVx * (-1.0 * oldSign);
+                    this.vy = newVy;
+                }
+            }
+
+            } else if (vx >= 0.0) {
+                if (this.rect.x + (vx * dt) + rect.width > rightPaddle.x) {
+                    if (this.rect.y + (vy * dt) > rightPaddle.y &&
+                            this.rect.y + (vy * dt) + this.rect.height < rightPaddle.y + rightPaddle.height) {
+                    double theta = calculateNewVelocityAngle(rightPaddle);
+                    double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
+                    double newVy = (-Math.sin(theta)) * Constants.BALL_SPEED;
+
+                    double oldSign = Math.signum(vx);
+                    this.vx = newVx * (-1.0 * oldSign);
+                    this.vy = newVy;
                     }
                 }
             }
-        }
 
-        this.rect.x += vx * Constants.BALL_SPEED * dt;
-        this.rect.y += vy * Constants.BALL_SPEED * dt;
+        this.rect.x += vx * dt;
+        this.rect.y += vy * dt;
     }
-
 }
--- Games/Pong/Constants.java
+++ Games/Pong/Constants.java
@@ -19,6 +19,8 @@ public class Constants {
     public static double TOOLBAR_HEIGHT;
     public static double INSETS_BOTTOM;
 
+    public static final double MAX_ANGLE = 45;
+
 
 
 
